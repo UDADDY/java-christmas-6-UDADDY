df2419d095091a6347807d50f4f5936ff7fc0a83
UDADDY <gustmd5715@gmail.com>
2023-11-12 18:04:22 +0900

    feat: 의존성 주입

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,17 @@
 package christmas;
 
+import christmas.controller.MarketController;
+import christmas.domain.Splitter;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+import java.text.DecimalFormat;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView(new Splitter());
+        OutputView outputView = new OutputView(new DecimalFormat("###,###"));
+        MarketController marketController = new MarketController(inputView, outputView);
+        marketController.run();
     }
 }
